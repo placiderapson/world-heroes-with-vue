60c712b165a94dd70a3062655b09be576a1841dd
Placide IRANDORA <placideirandora@gmail.com>
2019-12-19 21:22:48 +0200

    Get rid of the default template

--- heroes/src/App.vue
+++ heroes/src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 id="app__title">World's Well-Known Heroes</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
+
+  &__title {
+    text-transform: uppercase;
+  }
 }
 </style>
