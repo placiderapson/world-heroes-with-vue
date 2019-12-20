7017e89070f4376d0fd0176d75b04aa7ec172fcc
Placide IRANDORA <placideirandora@gmail.com>
2019-12-20 16:21:45 +0200

    Display the logo and improve the design

--- heroes/src/App.vue
+++ heroes/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <img src="./assets/superman.svg" alt="Heroes Logo">
     <h1 id="app__title">World's Well-Known Heroes</h1>
     <Heroes />
   </div>
@@ -23,10 +24,14 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #34495E;
-  margin: 50px auto;
+  margin: 3rem auto 10rem auto;
 
   &__title {
     text-transform: uppercase;
+  }
+
+  img {
+    width: 10rem;
   }
 }
 </style>
